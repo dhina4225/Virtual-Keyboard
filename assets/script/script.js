46c199a91a1fca326e19f9213d25d7940fe8adec
dina <Dinakaran0651@gmail.com>
2023-02-01 11:14:18 +0530

    update ui

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,6 @@
 
 const buttons = document.querySelectorAll(".btn");
 const textarea = document.querySelector("textarea");
-
 const delete_btn = document.querySelector(".delete");
 const shift_btn = document.querySelector(".shift");
 const space_btn = document.querySelector(".space");
